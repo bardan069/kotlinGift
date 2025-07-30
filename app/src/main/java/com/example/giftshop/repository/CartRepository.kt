5ddddddcc447264b887c149b99f43f5e34e3cb22
bardan096 <bardankunwar27@gmail.com>
2025-07-30 16:34:01 +0545

    order repo

--- app/src/main/java/com/example/giftshop/repository/CartRepository.kt
+++ app/src/main/java/com/example/giftshop/repository/CartRepository.kt
@@ -2,4 +2,9 @@ package com.example.giftshop.repository
 
 import com.example.giftshop.model.CartItemModel
 
-i
+interface CartRepository {
+    fun addToCart(item: CartItemModel, callback: (Boolean, String) -> Unit)
+    fun getCartItems(callback: (List<CartItemModel>, Boolean, String) -> Unit)
+    fun updateCartItemQuantity(cartItemId: String, quantity: Int, callback: (Boolean, String) -> Unit)
+    fun removeCartItem(cartItemId: String, callback: (Boolean, String) -> Unit)
+}
